b180994854669a199b1f085261b045e5a28d554f
GUYARD Marc <marc@mguyard.com>
2025-02-26 19:07:46 +0100

    fix(webhook): 🔗 Handle existing webhook subscriptions gracefully
    * Added a warning log when a webhook subscription already exists for a different URL.
    * The integration will now force update the `webhook_url` if a subscription conflict is detected.
    * Updated documentation to reflect the behavior when an existing subscription is found.

--- custom_components/diagral/__init__.py
+++ custom_components/diagral/__init__.py
@@ -161,6 +161,14 @@ async def register_webhook(
             except DiagralAPIError as err:
                 if "No subscription found for" in str(err):
                     pass
+                if "subscription already exists" in str(err):
+                    _LOGGER.warning(
+                        "A Webhook subscription already exists for another URL (%s). Integration will force update of webhook_url to %s",
+                        actual_webhook.webhook_url,
+                        webhook_url,
+                    )
+                    unregister_webhook(hass, entry, api, webhook_id, "register_webhook")
+                    register_webhook(hass, entry, api, "register_webhook")
                 else:
                     raise
 
