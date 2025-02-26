94bab40f14b894a8302ee32f6ac3606b5aabcb1c
GUYARD Marc <marc@mguyard.com>
2025-02-26 18:02:30 +0100

    feat(webhook): 🔗 Add webhook registration and unregistration action
    * Introduced `register_webhook` and `unregister_webhook` action.
    * Updated service names for arming and disarming groups.
    * Enhanced the `DiagralAlarmControlPanel` class with new action methods.

--- custom_components/diagral/const.py
+++ custom_components/diagral/const.py
@@ -10,7 +10,9 @@ CONF_SECRET_KEY = "secret_key"
 
 DEFAULT_SCAN_INTERVAL = 300
 
-SERVICE_ACTIVATE_GROUP = "arm_groups"
-SERVICE_DISABLE_GROUP = "disarm_groups"
+SERVICE_ARM_GROUP = "arm_groups"
+SERVICE_DISARMGROUP = "disarm_groups"
+SERVICE_REGISTER_WEBHOOK = "register_webhook"
+SERVICE_UNREGISTER_WEBHOOK = "unregister_webhook"
 
 INPUT_GROUPS = "group_ids"
